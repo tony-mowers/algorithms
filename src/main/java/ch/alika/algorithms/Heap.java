8319ad53107d50a0ca6116197233014e122b4c2a
Tony Mowers <tony.mowers@alika.ch>
2013-04-17 00:16:36 +0200

    refactored sort algorithms to be more readable

--- src/main/java/ch/alika/algorithms/Heap.java
+++ src/main/java/ch/alika/algorithms/Heap.java
@@ -6,9 +6,8 @@ public class Heap<T> {
 	private T[] data;
 	private int lastHeapIndex;
 	private Comparator<T> comparator;
-	
-	public Heap(T[] data,Comparator<T> c) 
-	{
+
+	public Heap(T[] data, Comparator<T> c) {
 		this.data = data;
 		this.comparator = c;
 		this.lastHeapIndex = data.length - 1;
@@ -16,15 +15,13 @@ public class Heap<T> {
 	}
 
 	public void sort() {
-		while (lastHeapIndex >= 1) 
-		{
-			swap(0,lastHeapIndex);
+		while (lastHeapIndex >= 1) {
+			swap(0, lastHeapIndex);
 			lastHeapIndex--;
 			heapify(0);
 		}
 	}
 
-	
 	private void swap(int i, int j) {
 		T temp = data[i];
 		data[i] = data[j];
@@ -36,41 +33,48 @@ public class Heap<T> {
 			heapify(i);
 		}
 	}
-	
-	private int parent(int i) {
-		return (i + 1) / 2 - 1;
-	}
 
-	private void heapify(int i) {
-		int highest = i;
-		int leftIndex = leftChildIndex(i);
-		int rightIndex = rightChildIndex(i);
-		if ( isHigherPriorityItem(i,leftIndex) )
-			highest = leftIndex;
-		
-		if ( isHigherPriorityItem(highest,rightIndex) )
-			highest = rightIndex;
-		
-		if (highest != i) {
-			swap(highest,i);
+	private void heapify(int rootIndex) {
+		int highest = highestPriorityItem(rootIndex, leftChildIndex(rootIndex),
+				rightChildIndex(rootIndex));
+
+		if (highest != rootIndex) {
+			swap(highest, rootIndex);
 			heapify(highest);
 		}
-		
+
+	}
+
+	private int highestPriorityItem(int i, int j, int k) {
+		int highest = i;
+
+		if (isHigherPriorityItem(i, j))
+			highest = j;
+
+		if (isHigherPriorityItem(highest, k))
+			highest = k;
+
+		return highest;
 	}
 
 	private boolean isInHeap(int i) {
-		return i <= lastHeapIndex; 
+		return i <= lastHeapIndex;
 	}
 
 	private boolean isHigherPriorityItem(int referenceIndex, int index) {
-		return isInHeap(index) && comparator.compare(data[index],data[referenceIndex]) == 1;
+		return isInHeap(index)
+				&& comparator.compare(data[index], data[referenceIndex]) == 1;
+	}
+
+	private int parent(int childIndex) {
+		return (childIndex + 1) / 2 - 1;
 	}
 
 	private int rightChildIndex(int i) {
-		return (i + 1)*2;
+		return (i + 1) * 2;
 	}
 
 	private int leftChildIndex(int i) {
-		return (i + 1)*2 - 1;
+		return (i + 1) * 2 - 1;
 	}
 }
