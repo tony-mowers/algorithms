54839fee37d30d1cf6884ae9f9b9846c050ecff6
Tony Mowers <tony.mowers@alika.ch>
2013-04-17 22:51:53 +0200

    add pop method to heap

---
+++ src/main/java/ch/alika/algorithms/HeapUnderflowException.java
@@ -0,0 +1,10 @@
+package ch.alika.algorithms;
+
+public class HeapUnderflowException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4618827386797467195L;
+
+}
---
+++ src/test/java/ch/alika/algorithms/HeapTest.java
@@ -0,0 +1,41 @@
+package ch.alika.algorithms;
+
+import static org.junit.Assert.*;
+import java.util.Comparator;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class HeapTest {
+	private Comparator<Integer> descending = new IsLessThan();
+
+	@Before
+	public void setUp() throws Exception {
+
+	}
+
+	@Test
+	public void testPop() {
+		Integer data[] = { 2, 8, 9, 37, 9 };
+		Heap<Integer> heap = new Heap<Integer>(data, descending);
+		assertEquals(5, heap.size());
+		assertEquals((Integer)2, heap.top());
+		assertEquals((Integer)2, heap.pop());
+		assertEquals(4, heap.size());
+		assertEquals((Integer)8,heap.pop());
+		assertEquals(3, heap.size());
+	}
+
+	private static final class IsLessThan implements Comparator<Integer> {
+		@Override
+		public int compare(Integer o1, Integer o2) {
+			if (o1 < o2)
+				return 1;
+
+			if (o1 == o2)
+				return 0;
+
+			return -1;
+		}
+	}
+}
