7169e5206ebadbbc452ce01df01f293d443bdc2a
Tony Mowers <tony.mowers@alika.ch>
2013-04-17 22:51:26 +0200

    add pop method to heap

--- src/main/java/ch/alika/algorithms/Sort.java
+++ src/main/java/ch/alika/algorithms/Sort.java
@@ -4,28 +4,28 @@ import java.util.Comparator;
 
 public class Sort {
 
-	public static <T> void insertionSort(T[] a, Comparator<T> c) {
-		for (int i = 1; i < a.length; i++) {
-			T key = a[i];
+	public static <T> void insertionSort(T[] data, Comparator<T> comparator) {
+		for (int i = 1; i < data.length; i++) {
+			T key = data[i];
 			int j = i - 1;
-			while (j >= 0 && c.compare(a[j], key) == 1) {
-				a[j + 1] = a[j];
+			while (j >= 0 && comparator.compare(data[j], key) == 1) {
+				data[j + 1] = data[j];
 				j--;
 			}
-			a[j + 1] = key;
+			data[j + 1] = key;
 		}
 	}
 
 	
-	public static <T> void heapSort(T[] data, Comparator<T> c) {
-		Heap<T> heap = new Heap<T>(data,c);
+	public static <T> void heapSort(T[] data, Comparator<T> comparator) {
+		Heap<T> heap = new Heap<T>(data,comparator);
 		while (heap.size() > 1) {
 			data[heap.size() - 1] = heap.pop();
 		}
 	}
 	
-	public static <T> void mergeSort(T[] a, Comparator<T> c) {
-		MergeSort.sort(a,c);
+	public static <T> void mergeSort(T[] a, Comparator<T> comparator) {
+		MergeSort.sort(a,comparator);
 	}
 
 }
